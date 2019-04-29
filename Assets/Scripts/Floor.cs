a3f26f4f778c6184368c195de7a849f9ba2992a1
ash <ash.s.fischer@gmail.com>
2019-04-29 18:44:23 -0500

    update all

---
+++ Assets/Scripts/Floor.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Floor : MonoBehaviour
+{
+    public float disAmount = 0.1f;
+    public GameObject target;
+    Vector3 distance;
+    float killDist;
+    Vector3 destroyDistance;
+    private Renderer mat;
+    
+    private void Start()
+    {
+        mat = GetComponent<Renderer>();
+        mat.material.SetTextureScale("_MainTex", new Vector2(Random.Range(-2f, -1f), Random.Range(1f, 2f)));
+    }
+    
+    private void Update()
+    {
+        killDist = target.transform.position.x - transform.position.x;
+        if (killDist > 0)
+        {
+            distance = target.transform.position - transform.position;
+            disAmount = (distance.magnitude / 2f) / 10;
+            mat.material.SetFloat("_Level", disAmount);
+        }
+
+
+        if(killDist > 11)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField]
+    private CharacterController2D controller;
+    [SerializeField]
+    private float speed;
+
+    private float horizontal;
+    
+    private bool jump;
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontal = Input.GetAxisRaw("Horizontal") * speed;
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        controller.Move(horizontal * Time.fixedDeltaTime, false, jump);
+        jump = false;
+    }
+}
